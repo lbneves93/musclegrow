b885f0a91cbc32fb4b09249134366ee327f47e0c
Leonardo Neves <leo.b.neves93@gmail.com>
2023-03-20 20:48:18 -0300

    Remove content ApplicationStop

--- scripts/ApplicationStop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /var/app/current
-echo > ../deploy.log
-echo Log in Docker Hub... >> ../deploy.log
-docker login --username lbneves --password redes002@
-echo Stopping all containers... >> ../deploy.log
-docker-compose down
-echo Removing all containers... >> ../deploy.log
-docker container prune -f
