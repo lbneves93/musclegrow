f71a07b2ee0787c9f1cb43272b8bcbfe8304e7da
Leonardo Neves <leo.b.neves93@gmail.com>
2023-03-20 19:28:30 -0300

    Log afterinstall hooks in appspec.

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 docker login --username lbneves --password redes002@
 cd /var/app/current
+echo > ../deploy.log
+echo Starting After Install... >> ../deploy.log
+echo Environment: $(printenv RAILS_ENV) >> ../deploy.log
+echo Building Container... >> ../deploy.log
 docker-compose build $(printenv RAILS_ENV)
+echo Migrating Database... >> ../deploy.log
 docker-compose run $(printenv RAILS_ENV) rails db:migrate
+echo Precompile Assets... >> ../deploy.log
 docker-compose run $(printenv RAILS_ENV) rails assets:precompile
 
 
