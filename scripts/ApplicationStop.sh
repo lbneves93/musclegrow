30c8c78c67bde212fd37115d5361703f5f6453b6
Leonardo Neves <leo.b.neves93@gmail.com>
2023-03-20 20:33:46 -0300

    Add logs applicationStop.

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cd /var/app/current
+echo > ../deploy.log
+echo Log in Docker Hub... >> ../deploy.log
 docker login --username lbneves --password redes002@
-docker stop $(docker ps -aq)
+echo Stopping all containers... >> ../deploy.log
+docker-compose down
+echo Removing all containers... >> ../deploy.log
 docker container prune -f
