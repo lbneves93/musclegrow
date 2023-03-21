00afa1008342e118e3c051457c37e90c9f6cf9ec
Leonardo Neves <leo.b.neves93@gmail.com>
2023-03-21 16:08:14 -0300

    Add bundle install in appspec.

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -5,6 +5,7 @@ docker login --username $dockerhub_user --password $dockerhub_pass
 cd /var/app/current
 app_env=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.RAILS_ENV')
 docker-compose build $app_env
+docker-compose run $app_env bundle
 docker-compose run $app_env rails db:migrate
 docker-compose run $app_env rails assets:precompile
 
