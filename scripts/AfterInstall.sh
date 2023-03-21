c50f22ad371eaef3bd5929a9df31cd52c785cddf
Leonardo Neves <leo.b.neves93@gmail.com>
2023-03-20 22:13:56 -0300

    Change env get in codedeploy hooks.

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 docker login --username lbneves --password redes002@
 cd /var/app/current
+app_env=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.RAILS_ENV')
 echo > ../deploy.log
 echo Starting After Install... >> ../deploy.log
-echo Environment: $(printenv RAILS_ENV) >> ../deploy.log
+echo Environment: $app_env >> ../deploy.log
 echo Building Container... >> ../deploy.log
-docker-compose build $(printenv RAILS_ENV)
+docker-compose build $app_env
 echo Migrating Database... >> ../deploy.log
-docker-compose run $(printenv RAILS_ENV) rails db:migrate
+docker-compose run $app_env rails db:migrate
 echo Precompile Assets... >> ../deploy.log
-docker-compose run $(printenv RAILS_ENV) rails assets:precompile
+docker-compose run $app_env rails assets:precompile
 
 
