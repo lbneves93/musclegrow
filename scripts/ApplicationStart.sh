5c6789bea0292dc82d3def4f9d0995dbf0ccf01f
Leonardo Neves <leo.b.neves93@gmail.com>
2023-03-21 16:18:54 -0300

    Stop all containers in appspec hooks.

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /var/app/current
 app_env=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.RAILS_ENV')
+docker-compose down
+docker container prune -f
 docker-compose up -d $app_env
--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /var/app/current
 docker-compose down
 docker container prune -f
