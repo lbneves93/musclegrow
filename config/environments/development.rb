9765adc0eaad09bd3b6d8fd08893b0d806fc5821
Leonardo Neves <leo.b.neves93@gmail.com>
2023-03-27 18:35:49 -0300

    Add Bootstrap.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Turn on Inline Source maps (so we can debug Rails 7 Bootstrap)
+  config.sass.inline_source_maps = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
