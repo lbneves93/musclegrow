21890c69c79310c766c1d09abd13ce5d38babbd8
Leonardo Neves <leo.b.neves93@gmail.com>
2023-04-25 11:21:18 -0300

    Fix deploy script to create rails master key file.

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+cd /var/app/current
+echo 'Create Rails Master Key File... Time:' $(date) >> ../deploy.log
+master_key=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.RAILS_MASTER_KEY')
+echo $(master_key) > config/master.key
 echo 'Log in Docker Hub... Time:' $(date) >> ../deploy.log
 dockerhub_user=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.DOCKERHUB_USER')
 dockerhub_pass=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.DOCKERHUB_PASS')
 docker login --username $dockerhub_user --password $dockerhub_pass
 echo 'Docker Hub logged... Time:' $(date) >> ../deploy.log
-cd /var/app/current
 app_env=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.RAILS_ENV')
 echo 'Docker Building container... Time:' $(date) >> ../deploy.log
 docker-compose build $app_env
